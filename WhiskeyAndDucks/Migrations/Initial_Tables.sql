5dfab0e80edd6ee4b30883d3ad0a351815ba2a8c
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-03-05 11:33:32 -0600

    sql table creation

---
+++ WhiskeyAndDucks/Migrations/Initial_Tables.sql
@@ -0,0 +1,20 @@
+﻿--Whiskey Table
+CREATE TABLE Whiskey (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(100) NOT NULL,
+    Price MONEY,
+    Proof INTEGER NOT NULL,
+    StockQty INTEGER DEFAULT 0,
+);
+
+--Reservation Table
+CREATE TABLE Reservation (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL,
+    [Type] INTEGER NOT NULL,
+    GroupSize INTEGER NOT NULL,
+    [Date] DATETIME2 NOT NULL,
+    ClubMember BIT NOT NULL,
+    WhiskeyId INTEGER,
+    CONSTRAINT FK_Whiskey_Reservation FOREIGN KEY(WhiskeyId) REFERENCES Whiskey(Id)
+);
